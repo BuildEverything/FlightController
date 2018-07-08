6a20a16001f8d364c04224778db745947189bae0
Tom <contact.tbrooks@gmail.com>
2018-07-07 20:56:01 -0400

    Set BMP180 test to high res

--- Tests/BMP180Test.py
+++ Tests/BMP180Test.py
@@ -34,7 +34,7 @@ import Adafruit_BMP.BMP085 as BMP085
 #
 # For the Beaglebone Black the library will assume bus 1 by default, which is
 # exposed with SCL = P9_19 and SDA = P9_20.
-sensor = BMP085.BMP085()
+#sensor = BMP085.BMP085()
 
 # Optionally you can override the bus number:
 #sensor = BMP085.BMP085(busnum=2)
@@ -43,7 +43,7 @@ sensor = BMP085.BMP085()
 # BMP085_STANDARD, BMP085_HIGHRES, or BMP085_ULTRAHIGHRES.  See the BMP085
 # datasheet for more details on the meanings of each mode (accuracy and power
 # consumption are primarily the differences).  The default mode is STANDARD.
-#sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
+sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
 
 print('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
 print('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
